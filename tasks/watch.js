f4f17a5f2db7eca1695a7408950553c1cf17cae3
Rémy Hannequin <remy.hannequin@gmail.com>
2014-07-17 15:51:05 +0100

    Improve content and styles

--- tasks/watch.js
+++ tasks/watch.js
@@ -6,5 +6,6 @@ module.exports = function(){
   gulp.watch(paths.sources.templates + '**/*.jade', ['html'])
   gulp.watch(paths.sources.stylesheets + '**/*.scss', ['styles'])
   gulp.watch(paths.sources.scripts + '**/*.js', ['scripts'])
+  gulp.watch(paths.sources.images + '**/*', ['images'])
   gulp.watch([paths.dist.root + '**/*']).on('change', server.livereload)
 }
