c0cae0e0aa7805d4ee379f7f2bf2011f2fe91d15
Rémy Hannequin <remy.hannequin@gmail.com>
2014-07-18 14:09:04 +0100

    Remove jQuery from project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,52 @@
-;(function(){
+;(function(win, doc){
   'use strict';
 
-  var $ = require('jquery')
+  var forEach = Array.prototype.forEach
 
-  $('body').addClass('js-enabled')
-  $('.js-project-title').on('click', projectTitleEvent)
+  // Notice CSS that JavaScript is enabled
+  doc.querySelector('body').className = 'js-enabled'
 
-  showProjectDescription($('.js-project-title:first'))
+  // Click event on projects title
+  forEach.call(doc.querySelectorAll('.js-project-title'), function(el) {
+    el.addEventListener('click', projectTitleEvent)
+  })
+
+  showProjectDescription(doc.querySelectorAll('.js-project-title')[0])
+
+
 
   function projectTitleEvent (e) {
     e.preventDefault()
-    var $el = $(e.currentTarget)
-    showProjectDescription($el)
+    var el = e.currentTarget
+    showProjectDescription(el)
   }
 
-  function showProjectDescription($el) {
-    var selector = $el.data('target')
-      , title = $el.html()
-      , link = $el.data('link')
-      , content = $(selector).html()
+  function showProjectDescription(el) {
+    var selector = el.getAttribute('data-target')
+      , title = el.innerHTML
+      , link = el.getAttribute('data-link')
+      , content = doc.querySelector(selector).innerHTML
       , htmlTitle = '<h2><a href="' + link + '">' + title + '</a></h2>'
-    addCurrentClass($el)
-    $('.project-descriptions').html(htmlTitle + content)
+    addCurrentClass(el)
+    doc.querySelector('.project-descriptions').innerHTML = htmlTitle + content
   }
 
-  function addCurrentClass($el) {
+  function addCurrentClass(selector) {
     var currentClassName = 'current-project'
-    $('.' + currentClassName).removeClass(currentClassName)
-    $el.addClass(currentClassName)
+      , selectorClassName = selector.className
+    removeClass(currentClassName)
+    if(selector.className.indexOf(currentClassName) === -1) {
+      selector.className = selectorClassName + ' ' + currentClassName
+    }
   }
 
-})()
+  function removeClass(className) {
+    var elementsWithClass = doc.querySelectorAll('.' + className)
+    if(elementsWithClass.length > 0) {
+      forEach.call(elementsWithClass, function(el) {
+        el.className = el.className.split(className).join('').trim()
+      })
+    }
+  }
+
+})(window, window.document)
