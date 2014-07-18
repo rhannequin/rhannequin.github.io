a89dfe20e3a754e2b12804d7526699ae06b30188
Rémy Hannequin <remy.hannequin@gmail.com>
2014-07-18 13:27:26 +0100

    Show project description on click

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,33 @@
 ;(function(){
   'use strict';
 
+  var $ = require('jquery')
+
+  $('body').addClass('js-enabled')
+  $('.js-project-title').on('click', projectTitleEvent)
+
+  showProjectDescription($('.js-project-title:first'))
+
+  function projectTitleEvent (e) {
+    e.preventDefault()
+    var $el = $(e.currentTarget)
+    showProjectDescription($el)
+  }
+
+  function showProjectDescription($el) {
+    var selector = $el.data('target')
+      , title = $el.html()
+      , link = $el.data('link')
+      , content = $(selector).html()
+      , htmlTitle = '<h2><a href="' + link + '">' + title + '</a></h2>'
+    addCurrentClass($el)
+    $('.project-descriptions').html(htmlTitle + content)
+  }
+
+  function addCurrentClass($el) {
+    var currentClassName = 'current-project'
+    $('.' + currentClassName).removeClass(currentClassName)
+    $el.addClass(currentClassName)
+  }
+
 })()
