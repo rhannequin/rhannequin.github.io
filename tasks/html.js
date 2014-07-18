4060315b3296d20f18e07b93aded14f2f64db325
Rémy Hannequin <remy.hannequin@gmail.com>
2014-07-18 15:33:51 +0100

    Use JSON file to automate projects

--- tasks/html.js
+++ tasks/html.js
@@ -1,11 +1,12 @@
-var gulp    = require('gulp')
-  , jade    = require('gulp-jade')
-  , plumber = require('gulp-plumber')
-  , paths   = require('./paths')
+var gulp     = require('gulp')
+  , jade     = require('gulp-jade')
+  , plumber  = require('gulp-plumber')
+  , paths    = require('./paths')
+  , projects = require('../' + paths.sources.templates + 'projects.json')
 
 module.exports = function() {
   return gulp.src(paths.sources.templates + 'index.jade')
     .pipe(plumber())
-    .pipe(jade({ pretty: true }))
+    .pipe(jade({ pretty: true, locals: { projects: projects } }))
     .pipe(gulp.dest(paths.dist.root))
 }
