67719561463610ed5d05f832881bd660ce9b6026
Rémy Hannequin <remy.hannequin@gmail.com>
2014-08-21 22:52:55 +0200

    Change background and add minify tasks to assets

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,6 +1,6 @@
 var gulp       = require('gulp')
   , plumber    = require('gulp-plumber')
-  // , uglify     = require('gulp-uglify')
+  , uglify     = require('gulp-uglify')
   , browserify = require('gulp-browserify')
   , paths      = require('./paths')
 
@@ -8,5 +8,6 @@ module.exports = function() {
   return gulp.src(paths.sources.scripts + 'main.js')
     .pipe(plumber())
     .pipe(browserify())
+    .pipe(uglify())
     .pipe(gulp.dest(paths.dist.scripts))
 }
--- tasks/styles.js
+++ tasks/styles.js
@@ -2,6 +2,7 @@ var gulp         = require('gulp')
   , plumber      = require('gulp-plumber')
   , sass         = require('gulp-ruby-sass')
   , autoprefixer = require('gulp-autoprefixer')
+  , minifyCSS    = require('gulp-minify-css')
   , paths        = require('./paths')
 
 module.exports = function() {
@@ -10,5 +11,6 @@ module.exports = function() {
     .pipe(plumber())
     .pipe(sass({ style: 'expanded', loadPath: [__dirname + '/../' + sassDir] }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(minifyCSS())
     .pipe(gulp.dest(paths.dist.stylesheets))
 }
