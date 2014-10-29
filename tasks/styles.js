cb51bce3d9d0d23cc0613611963f60aa3b9005fb
Rémy Hannequin <remy.hannequin@gmail.com>
2014-10-29 23:03:38 +0100

    Split main.scss into several files with good practice architecture

--- tasks/styles.js
+++ tasks/styles.js
@@ -15,6 +15,6 @@ module.exports = function() {
       , image: paths.sources.images
     }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(minifyCSS())
+    // .pipe(minifyCSS())
     .pipe(gulp.dest(paths.dist.stylesheets))
 }
